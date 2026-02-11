ce985ec4d986505e76692fecb57473ae208bc1d7
Jason Park <142364450+Voyager466920@users.noreply.github.com>
2026-02-11 09:58:03 +0900

    Time: 38 ms (20.86%), Space: 14 MB (79.63%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        g.sort()
+        s.sort()
+
+        i = 0
+        j = 0
+        total = 0
+
+        while i != len(g) and j != len(s):
+            if g[i] <= s[j]:
+                i += 1
+                j += 1
+                total += 1
+            elif g[i] > s[j]:
+                j += 1
+            else:
+                i += 1
+        return total
